22493dcdf9320b47ec866f992b56b393cd93658b
Caleb Epstein <caleb@volanttrading.com>
2016-06-15 09:53:22 -0400

    helm binding tweaks

--- personal/caleb.el
+++ personal/caleb.el
@@ -38,12 +38,18 @@
 (global-set-key (kbd "<f8>") 'vc-dir)
 (global-set-key (kbd "<XF86MailForward>") 'vc-dir)
 
+(require 'prelude-helm-everywhere)
 (global-set-key (kbd "C-<return>") 'helm-M-x)
-(define-key prelude-mode-map (kbd "s-g") nil)
-
-(setq helm-command-prefix-key "C-c h")
-(global-set-key (kbd "C-x b") 'helm-mini)
+;; (setq helm-command-prefix-key "C-c h")
+;; (global-set-key (kbd "C-x b") 'helm-mini)
 ;; (global-set-key (kbd "M-x") 'helm-M-x)
+
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
+(define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
+
+
+(define-key prelude-mode-map (kbd "s-g") nil)
 
 ;; Smex all the things!
 ;; (require 'smex) ; Not needed if you use package.el
