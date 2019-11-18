281599a70c48d0990dcfee029599876606c6a4c6
Caleb Epstein <caleb.epstein@volanttrading.com>
2019-11-18 09:12:33 -0500

    No more helm-map

--- personal/caleb.el
+++ personal/caleb.el
@@ -49,9 +49,9 @@
 ;; (global-set-key (kbd "C-x b") 'helm-mini)
 ;; (global-set-key (kbd "M-x") 'helm-M-x)
 
-(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
-(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
-(define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
+;; (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
+;; (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
+;; (define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
 
 
 (define-key prelude-mode-map (kbd "s-g") nil)
